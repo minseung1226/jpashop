d835002d9ce98327bdec768c9e141be2125cf1e6
minseung1226 <minseung1226@naver.com>
2023-02-15 23:42:00 +0900

    JPA 실전 프로젝트 -주문,주문상품 엔티티 개발, 주문 리포지토리 개발 ,주문 서비스 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -47,5 +47,37 @@ public class Order {
         delivery.setOrder(this);
     }
 
+    public static Order createOrder(Member member,Delivery delivery,OrderItem... orderItems){
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    public void cancel(){
+        if(delivery.getStatus()==DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송이 완료된 상품은 취소 불가");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    public int getTotalPrice(){
+        int totalPrice=0;
+
+        for (OrderItem orderItem : orderItems) {
+            totalPrice+=orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
+
 
 }
---
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,53 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.Repository.ItemRepository;
+import jpabook.jpashop.Repository.MemberRepository;
+import jpabook.jpashop.Repository.OrderRepository;
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional(readOnly = true)
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+    @Transactional
+    public Long order(Long memberId,Long itemId,int count){
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        //배송정보
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        //주문 상품생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        //주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        orderRepository.save(order);
+
+        return order.getId();
+    }
+
+    @Transactional
+    public void cancelOrder(Long orderId){
+        Order order = orderRepository.findOne(orderId);
+        order.cancel();
+    }
+/*
+    public List<Order> findOrders(OrderSearch orderSearch){
+        return orderRepository.findAll(orderSearch);
+    }*/
+}
